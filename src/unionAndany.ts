9a13c82a78c4082a71a87ddb48a89405e48c0315
Fahim2023 <fahimshahrier2023@gmail.com>
2026-05-31 13:34:24 +0600

    creating the unionAndany.ts file in the src

---
+++ src/unionAndany.ts
@@ -0,0 +1,17 @@
+let subs: number | string = "1M";
+
+let apiRequestStatus: "pending" | "success" | "error" = "pending";
+
+apiRequestStatus = "success";
+
+let airlineSeat: "aisle" | "window" | "middle" = "aisle";
+
+airlineSeat = "aisle";
+
+const orders = ["12", "20", "28", "42"];
+
+let currentorder;
+
+// for(let order of orders){
+//     if(order==="")
+// }
