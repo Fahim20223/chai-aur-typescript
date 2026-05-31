cdd4f0ab86d29bb4bde96e6fa38519dfa9d8442b
Fahim2023 <fahimshahrier2023@gmail.com>
2026-05-31 13:07:33 +0600

    added the file typesInTs.ts in the src & writing the code of type inference & annotation

---
+++ src/typesInTs.ts
@@ -0,0 +1,12 @@
+let drink = "chai";
+
+// type inference means typescript understands by it's what is it's type either it's string or boolean whatever
+let cups = Math.random() > 0.5 ? 10 : 5;
+let channelName = "Chaiaurcode";
+
+// Type annotation means => bolte hobeje eta type er e hobeh eta string ba boolean erkom
+let chaiFlavour: string = "masala chai";
+chaiFlavour = "Ginger tea";
+// chaiFlavour = 2;
+
+let chaiOrder: boolean;
